34411b652c6b9535bcc848c1c63e7271f95f4195
Whirvis T. Wheatley <whirvis@ardenus.org>
2016-08-08 20:01:14 -0500

    Started on 2.0
    I decided to delete pretty much all the code to start on v2.0!

--- src/main/java/net/marfgamer/raknet/client/RakNetClient.java
+++
@@ -1,9 +0,0 @@
-package net.marfgamer.raknet.client;
-
-public class RakNetClient {
-	
-	public RakNetClient(int maxTransferUnit, long serverTimeout) {
-		
-	}
-	
-}
--- src/main/java/net/marfgamer/raknet/event/Event.java
+++
@@ -1,4 +0,0 @@
-package net.marfgamer.raknet.event;
-
-public class Event {
-}
