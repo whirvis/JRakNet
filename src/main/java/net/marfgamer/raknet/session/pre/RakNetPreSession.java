d92e952c99e39ec59bf211e5e9f5f25fc3dcdeca
Trent <trent0308@live.com>
2016-09-21 21:29:12 -0500

    MINECON
    *heavy breathing of excitement*

--- src/main/java/net/marfgamer/raknet/session/pre/RakNetPreSession.java
+++ src/main/java/net/marfgamer/raknet/session/pre/RakNetPreSession.java
@@ -8,9 +8,12 @@ import net.marfgamer.raknet.Packet;
 
 public class RakNetPreSession {
 
+	public static final long TIMEOUT_MILLIS = 5000L;
+
 	private final InetSocketAddress address;
 	private final Channel channel;
 	private PreSessionStatus status;
+	private long lastReceiveTime;
 
 	public RakNetPreSession(InetSocketAddress address, Channel channel) {
 		this.address = address;
@@ -23,6 +26,14 @@ public class RakNetPreSession {
 
 	public void setStatus(PreSessionStatus status) {
 		this.status = status;
+	}
+
+	public long getLastReceiveTime() {
+		return this.lastReceiveTime;
+	}
+
+	public void updateLastReceiveTime() {
+		this.lastReceiveTime = System.currentTimeMillis();
 	}
 
 	public void sendPacket(Packet packet) {
