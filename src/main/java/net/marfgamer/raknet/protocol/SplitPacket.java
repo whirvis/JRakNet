5127810d770e0d2584daf2c6085a384b40665e84
Trent <trent0308@live.com>
2016-10-13 21:56:35 -0500

    Progress!
    It seems that RakNetSessions are very close to working! I have dome some
    tests that have succeeded but they were minimal. I also managed to get
    the login sequence working for the server! I also started working the
    event system some more, it's kind of awkard but I can try and fix it up
    so it seems more natural. There is also a glitch where when a session is
    registered into the HashMap a ConcurrentModificationException is thrown.
    I will try to fix this tommorow :)

--- src/main/java/net/marfgamer/raknet/protocol/SplitPacket.java
+++ src/main/java/net/marfgamer/raknet/protocol/SplitPacket.java
@@ -8,35 +8,34 @@ public class SplitPacket {
 
 	private final int splitId;
 	private final int splitCount;
+	private final Reliability reliability;
 
 	private final IntMap<Packet> payloads;
 
-	public SplitPacket(int splitId, int splitCount) {
+	public SplitPacket(int splitId, int splitCount, Reliability reliability) {
 		this.splitId = splitId;
 		this.splitCount = splitCount;
+		this.reliability = reliability;
 		this.payloads = new IntMap<Packet>();
 	}
 
-	public EncapsulatedPacket update(EncapsulatedPacket encapsulated) {
+	public Packet update(EncapsulatedPacket encapsulated) {
 		// Update payload data
 		if (encapsulated.split != true || encapsulated.splitId != this.splitId
-				|| encapsulated.splitCount != this.splitCount) {
+				|| encapsulated.splitCount != this.splitCount || encapsulated.reliability != this.reliability) {
 			throw new IllegalArgumentException("This split packet does not belong to this one!");
 		}
 		payloads.put(encapsulated.splitIndex, encapsulated.payload);
 
 		// If the map is large enough then put the packet together and return it
 		if (payloads.size() >= this.splitCount) {
-			EncapsulatedPacket finalEncapsulated = new EncapsulatedPacket();
-			// TODO
-			
 			Packet finalPayload = new Packet();
 			for (int i = 0; i < payloads.size(); i++) {
 				finalPayload.write(payloads.get(i).array());
 			}
-			
+			return finalPayload;
 		}
-		
+
 		// The packet is not yet ready
 		return null;
 	}
