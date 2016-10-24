e9044cd33df818016f747a22e3e56dc32b3b3427
Whirvis T. Wheatley <whirvis@ardenus.org>
2016-10-24 18:53:52 -0500

    Almost done with JRakNet v2.0!
    I'm almost done, all I need to do left is write up some tests so I can
    make sure systems are working properly :)

--- src/main/java/net/marfgamer/raknet/session/RakNetClientSession.java
+++ src/main/java/net/marfgamer/raknet/session/RakNetClientSession.java
@@ -1,3 +1,33 @@
+/*
+ *       _   _____            _      _   _          _   
+ *      | | |  __ \          | |    | \ | |        | |  
+ *      | | | |__) |   __ _  | | __ |  \| |   ___  | |_ 
+ *  _   | | |  _  /   / _` | | |/ / | . ` |  / _ \ | __|
+ * | |__| | | | \ \  | (_| | |   <  | |\  | |  __/ | |_ 
+ *  \____/  |_|  \_\  \__,_| |_|\_\ |_| \_|  \___|  \__|
+ *                                                  
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 MarfGamer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.  
+ */
 package net.marfgamer.raknet.session;
 
 import java.net.InetSocketAddress;
@@ -69,9 +99,9 @@ public class RakNetClientSession extends RakNetSession {
 
 	@Override
 	public void handlePacket(RakNetPacket packet, int channel) {
-		short id = packet.getId();
-		System.out.println(id);
-		if (id == MessageIdentifier.ID_CONNECTION_REQUEST && this.getState() == RakNetState.DISCONNECTED) {
+		short packetId = packet.getId();
+
+		if (packetId == MessageIdentifier.ID_CONNECTION_REQUEST && this.getState() == RakNetState.DISCONNECTED) {
 			ConnectionRequest request = new ConnectionRequest(packet);
 			request.decode();
 
@@ -93,13 +123,13 @@ public class RakNetClientSession extends RakNetSession {
 				this.setState(RakNetState.DISCONNECTED);
 				server.removeSession(this, "Connection failed, invalid GUID");
 			}
-		} else if (id == MessageIdentifier.ID_NEW_INCOMING_CONNECTION && this.getState() == RakNetState.HANDSHAKING) {
+		} else if (packetId == MessageIdentifier.ID_NEW_INCOMING_CONNECTION
+				&& this.getState() == RakNetState.HANDSHAKING) {
 			NewIncomingConnection clientHandshake = new NewIncomingConnection(packet);
 			clientHandshake.decode();
 
 			if (clientHandshake.serverTimestamp == server.getTimestamp()) {
 				this.timestamp = (System.currentTimeMillis() - clientHandshake.clientTimestamp);
-
 				this.setState(RakNetState.CONNECTED);
 				server.getListener().onClientConnect(this);
 			} else {
@@ -110,12 +140,12 @@ public class RakNetClientSession extends RakNetSession {
 				this.setState(RakNetState.DISCONNECTED);
 				server.removeSession(this, "Connection failed, invalid timestamp");
 			}
-		} else if (id == MessageIdentifier.ID_DISCONNECTION_NOTIFICATION) {
+		} else if (packetId == MessageIdentifier.ID_DISCONNECTION_NOTIFICATION) {
 			DisconnectionNotification disconnectionNotification = new DisconnectionNotification(packet);
 			disconnectionNotification.decode();
 
 			server.removeSession(this, "Client disconnected");
-		} else if (id >= MessageIdentifier.ID_USER_PACKET_ENUM) {
+		} else if (packetId >= MessageIdentifier.ID_USER_PACKET_ENUM) {
 			server.getListener().handlePacket(this, packet, channel);
 		}
 	}
