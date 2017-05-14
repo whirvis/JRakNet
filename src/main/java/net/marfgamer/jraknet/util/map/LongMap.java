297f212c74ee117aee24bbc754c9991a016f1790
Whirvis T. Wheatley <whirvis@ardenus.org>
2017-05-14 18:04:54 -0500

    Updated some javadocs
    The custom map classes didn't have any documentation for them, so I made
    some (mostly copying the Java HashMap docs with minor changes) and
    redeployed version 2.6.3 with those docs since no code was changed.
    Don't worry if your maven plugin doesn't update the jars for 2.6.3, as
    no actual code was changed except for some documentation.

--- src/main/java/net/marfgamer/jraknet/util/map/LongMap.java
+++ src/main/java/net/marfgamer/jraknet/util/map/LongMap.java
@@ -33,26 +33,95 @@ package net.marfgamer.jraknet.util.map;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This class is used for using longs as keys in normal maps without having to
+ * worry about boxing them.
+ *
+ * @author MarfGamer
+ */
 public class LongMap<T> extends HashMap<Long, T> implements Map<Long, T>, DynamicKey<Long> {
 
 	private static final long serialVersionUID = 4324132003573381634L;
 
+	/**
+	 * Returns <tt>true</tt> if this map contains a mapping for the specified
+	 * key.
+	 *
+	 * @param key
+	 *            The key whose presence in this map is to be tested
+	 * @return <tt>true</tt> if this map contains a mapping for the specified
+	 *         key.
+	 */
 	public boolean containsKey(long key) {
 		return super.containsKey(new Long(key));
 	}
 
+	/**
+	 * Returns <tt>true</tt> if this map maps one or more keys to the specified
+	 * value.
+	 *
+	 * @param value
+	 *            value whose presence in this map is to be tested
+	 * @return <tt>true</tt> if this map maps one or more keys to the specified
+	 *         value
+	 */
 	public boolean containsValue(Object value) {
 		return super.containsValue(value);
 	}
 
+	/**
+	 * Returns the value to which the specified key is mapped, or {@code null}
+	 * if this map contains no mapping for the key.
+	 *
+	 * <p>
+	 * More formally, if this map contains a mapping from a key {@code k} to a
+	 * value {@code v} such that {@code (key==null ? k==null :
+	 * key.equals(k))}, then this method returns {@code v}; otherwise it returns
+	 * {@code null}. (There can be at most one such mapping.)
+	 *
+	 * <p>
+	 * A return value of {@code null} does not <i>necessarily</i> indicate that
+	 * the map contains no mapping for the key; it's also possible that the map
+	 * explicitly maps the key to {@code null}. The {@link #containsKey
+	 * containsKey} operation may be used to distinguish these two cases.
+	 *
+	 * @see #put(long, Object)
+	 * @param key
+	 *            they key the value is mapped to.
+	 * @return the value to which the specified key is mapped.
+	 */
 	public T get(long key) {
 		return super.get(new Long(key));
 	}
 
+	/**
+	 * Associates the specified value with the specified key in this map. If the
+	 * map previously contained a mapping for the key, the old value is
+	 * replaced.
+	 *
+	 * @param key
+	 *            key with which the specified value is to be associated
+	 * @param value
+	 *            value to be associated with the specified key
+	 * @return the previous value associated with <tt>key</tt>, or <tt>null</tt>
+	 *         if there was no mapping for <tt>key</tt>. (A <tt>null</tt> return
+	 *         can also indicate that the map previously associated
+	 *         <tt>null</tt> with <tt>key</tt>.)
+	 */
 	public T put(long key, T value) {
 		return super.put(new Long(key), value);
 	}
 
+	/**
+	 * Removes the mapping for the specified key from this map if present.
+	 *
+	 * @param key
+	 *            key whose mapping is to be removed from the map
+	 * @return the previous value associated with <tt>key</tt>, or <tt>null</tt>
+	 *         if there was no mapping for <tt>key</tt>. (A <tt>null</tt> return
+	 *         can also indicate that the map previously associated
+	 *         <tt>null</tt> with <tt>key</tt>.)
+	 */
 	public T remove(long key) {
 		return super.remove(new Long(key));
 	}
