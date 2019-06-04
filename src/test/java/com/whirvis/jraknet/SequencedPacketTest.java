276e49b07f77aa617ec3cfd09e4ee61c25ed8ebe
Trent Summerlin <whirvis@ardenus.org>
2019-06-04 12:34:53 -0500

    Update SequencedPacketTest to address #111

--- src/test/java/com/whirvis/jraknet/SequencedPacketTest.java
+++ src/test/java/com/whirvis/jraknet/SequencedPacketTest.java
@@ -31,7 +31,9 @@ package com.whirvis.jraknet;
 
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -59,7 +61,8 @@ import com.whirvis.jraknet.server.RakNetServerListener;
 public final class SequencedPacketTest {
 
 	private static final Logger LOG = LogManager.getLogger(SequencedPacketTest.class);
-	private static final short SEQUENCE_START_ID = 0xFE;
+	private static final short SEQUENCE_HEADER_ID = 0xFD;
+	private static final short SEQUENCE_END_ID = 0xFE;
 	private static final int PACKET_SEND_COUNT = 1000;
 	private static long startSend = -1;
 	private static int packetReceiveCount = 0;
@@ -76,30 +79,14 @@ public final class SequencedPacketTest {
 	 *            the program arguments. These values are ignored.
 	 * @throws RakNetException
 	 *             if a RakNet error occurs.
-	 * @throws InterruptedException
-	 *             if any thread has interrupted the current thread. The
-	 *             <i>interrupted status</i> of the current thread is cleared
-	 *             when this exception is thrown.
 	 * @throws UnknownHostException
 	 *             if the <code>localhost</code> address could not be found.
 	 */
-	public static void main(String[] args) throws RakNetException, InterruptedException, UnknownHostException {
+	public static void main(String[] args) throws RakNetException, UnknownHostException {
 		LOG.info("Creating server, sleeping for 3000MS, and then creating the client...");
 		createServer();
 		RakNet.sleep(3000L);
 		createClient();
-
-		// Wait for either a result or for a timeout
-		long currentTime = System.currentTimeMillis();
-		while (true) {
-			Thread.sleep(0, 1); // Lower CPU usage
-			if (currentTime - startSend >= 30000 && startSend > -1) {
-				LOG.info("Failed to complete test due to timeout (Took over 30 seconds!), printing results...");
-				printResults();
-				System.exit(1);
-			}
-			currentTime = System.currentTimeMillis();
-		}
 	}
 
 	/**
@@ -110,6 +97,7 @@ public final class SequencedPacketTest {
 	 *             if a RakNet error occurs.
 	 */
 	private static RakNetServer createServer() throws RakNetException {
+		AtomicInteger lastPacketIndex = new AtomicInteger(-1);
 		RakNetServer server = new RakNetServer(RakNetTest.WHIRVIS_DEVELOPMENT_PORT, 1);
 		server.addListener(new RakNetServerListener() {
 
@@ -131,10 +119,15 @@ public final class SequencedPacketTest {
 
 			@Override
 			public void handleMessage(RakNetServer server, RakNetClientPeer peer, RakNetPacket packet, int channel) {
-				int packetIndex = packet.readInt();
-				packetReceiveCount++;
-				packetsReceived[packetIndex] = true;
-				if (packetIndex >= PACKET_SEND_COUNT - 1) {
+				if (packet.getId() == SEQUENCE_HEADER_ID) {
+					int packetIndex = packet.readInt();
+					if (packetIndex - lastPacketIndex.intValue() > 1) {
+						LOG.info("Lost packets " + (lastPacketIndex.intValue() + 1) + "-" + (lastPacketIndex.intValue() + (packetIndex - lastPacketIndex.intValue()) - 1));
+					}
+					lastPacketIndex.set(packetIndex);
+					packetReceiveCount++;
+					packetsReceived[packetIndex] = true;
+				} else if (packet.getId() == SEQUENCE_END_ID) {
 					printResults();
 					System.exit(0);
 				}
@@ -170,11 +163,12 @@ public final class SequencedPacketTest {
 				int packetSize = 0;
 				startSend = System.currentTimeMillis();
 				for (int i = 0; i < PACKET_SEND_COUNT; i++) {
-					RakNetPacket sequencedPacket = new RakNetPacket(SEQUENCE_START_ID);
+					RakNetPacket sequencedPacket = new RakNetPacket(SEQUENCE_HEADER_ID);
 					sequencedPacket.writeInt(i);
 					packetSize += sequencedPacket.size();
 					peer.sendMessage(Reliability.UNRELIABLE_SEQUENCED, sequencedPacket);
 				}
+				peer.sendMessage(Reliability.RELIABLE_SEQUENCED, SEQUENCE_END_ID);
 				LOG.info("Client - Sent " + PACKET_SEND_COUNT + " packets (" + packetSize + " bytes, " + (packetSize / 4) + " ints)");
 			}
 
@@ -200,8 +194,7 @@ public final class SequencedPacketTest {
 	 */
 	private static void printResults() {
 		LOG.info("Server - Sequenced packet test finished, lost "
-				+ (packetReceiveCount >= PACKET_SEND_COUNT ? "no"
-						: Float.toString(((float) PACKET_SEND_COUNT - packetReceiveCount / (float) PACKET_SEND_COUNT) * 100).substring(0, 3).replace(".", "") + "% of")
+				+ (packetReceiveCount >= PACKET_SEND_COUNT ? "no" : new DecimalFormat("##.##").format(100.0F - (((float) packetReceiveCount / PACKET_SEND_COUNT) * 100F)) + "% of")
 				+ " packets (Took " + (System.currentTimeMillis() - startSend) + "MS)");
 		if (packetReceiveCount < PACKET_SEND_COUNT) {
 			ArrayList<Integer> lostPackets = new ArrayList<Integer>();
