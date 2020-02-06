f18c0ff6c1b7f39b8542573b7eeb0fecbb2e47c8
Trent Summerlin <whirvis@ardenus.org>
2020-02-06 17:17:20 -0600

    Update copyright years

--- src/main/java/com/whirvis/jraknet/protocol/ConnectionType.java
+++ src/main/java/com/whirvis/jraknet/protocol/ConnectionType.java
@@ -7,7 +7,7 @@
  *
  * the MIT License (MIT)
  *
- * Copyright (c) 2016-2019 Trent Summerlin
+ * Copyright (c) 2016-2020 Trent Summerlin
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -43,7 +43,6 @@ import java.util.UUID;
  * type protocol are:
  * <ul>
  * <li>JRakNet by Trent Summerlin</li>
- * <li>RakLib by PocketMine-MP</li>
  * </ul>
  * 
  * @author Trent Summerlin
